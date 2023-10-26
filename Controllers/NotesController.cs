c22dc708230e01c23360d5ada8b85d2648576c91
M Faisal Hayat <m.faisalhayat3@gmail.com>
2023-10-26 20:38:57 +0500

    GetAll() for bote app has been implemented

---
+++ Controllers/NotesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NoteApp.Data;
+
+namespace NoteApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NotesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public NotesController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            // get all notes from database
+            if (_dbContext.Notes != null)
+            {
+                var obj = await _dbContext.Notes.ToListAsync();
+                return Ok(obj);
+            }
+            else
+            {
+                return BadRequest("No Notes exist in database.");
+            }
+        }
+    }
+}
